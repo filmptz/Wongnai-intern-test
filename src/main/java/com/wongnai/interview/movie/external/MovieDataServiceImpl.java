e4f272a2b5d422d76be19e338252750f5248e677
filmptz <parichaya35@gmail.com>
2021-01-04 21:35:16 +0700

    Update MovieDataServiceImpl.java

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -3,13 +3,18 @@ package com.wongnai.interview.movie.external;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
-	public static final String MOVIE_DATA_URL
-			= "https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json";
+	public static final String MOVIE_DATA_URL = "https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json";
 
 	@Autowired
 	private RestOperations restTemplate;
@@ -19,11 +24,23 @@ public class MovieDataServiceImpl implements MovieDataService {
 
 	@Override
 	public MoviesResponse fetchAll() {
-		//TODO:
-		// Step 1 => Implement this method to download data from MOVIE_DATA_URL and fix any error you may found.
-		// Please noted that you must only read data remotely and only from given source,
+		// TODO:
+		// Step 1 => Implement this method to download data from MOVIE_DATA_URL and fix
+		// any error you may found.
+		// Please noted that you must only read data remotely and only from given
+		// source,
 		// do not download and use local file or put the file anywhere else.
-
-		return null;
+		URL url;
+		objectMapper = new ObjectMapper();
+		MoviesResponse movie = null;
+		try {
+			url = new URL(MOVIE_DATA_URL);
+			movie = objectMapper.readValue(url, MoviesResponse.class);
+		} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		return movie;
 	}
 }
